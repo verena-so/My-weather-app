89bb783f317a3ee4a915088fdcfe81fde46528bc
verena-so <151364839+verena-so@users.noreply.github.com>
2024-01-18 15:04:57 +0100

    Injected more weather data

--- src/index.js
+++ src/index.js
@@ -1,10 +1,47 @@
 function refreshWeather(response) {
   let temperatureElement = document.querySelector("#temperature");
   let temperatureOutput = response.data.temperature.current;
-  let cityElement = document.querySelector("#city");
-
-  cityElement.innerHTML = response.data.city;
   temperatureElement.innerHTML = Math.round(temperatureOutput);
+
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+
+  let conditionElement = document.querySelector("#weather-condition");
+  let conditionOutput = response.data.condition.description;
+  conditionElement.innerHTML = conditionOutput;
+
+  let humidityElement = document.querySelector("#weather-humidity");
+  let humidityOutput = response.data.temperature.humidity;
+  humidityElement.innerHTML = `${response.data.temperature.humidity} %`;
+
+  let windElement = document.querySelector("#weather-wind");
+  let windOutput = response.data.wind.speed;
+  windElement.innerHTML = `${response.data.wind.speed} km/h`;
+
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
+  timeElement.innerHTML = formatDate(date);
+}
+
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day}, ${hours}:${minutes}`;
 }
 
 function searchCity(city) {
