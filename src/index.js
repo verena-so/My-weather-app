6d20d8542e49b2f80b662c4e01e5a64202bae8fd
verena-so <151364839+verena-so@users.noreply.github.com>
2024-01-18 14:18:50 +0100

    Added search functionality using API

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperatureOutput = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperatureOutput);
+}
+
+function searchCity(city) {
+  let apiKey = "57eafdf333f32b7f37o90e19f0tdeb44";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(refreshWeather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
 
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#city");
-
   cityElement.innerHTML = searchInput.value;
+
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
-
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Vienna");
